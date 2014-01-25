a7da1d69dab9428af87e30b8c2805cdb5fdffbf0
Steve Castle <steve@tigerhaunt.com>
2014-01-25 13:13:14 -0800

    Created Detail Views

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/clock', {templateUrl: 'partials/partial1.html', controller: 'EmployeeList'});
-  $routeProvider.when('/cards', {templateUrl: 'partials/partial2.html', controller: 'TimeCards'});
+  $routeProvider.when('/clock', {templateUrl: 'partials/clock.html', controller: 'EmployeeList'});
+  $routeProvider.when('/cards', {templateUrl: 'partials/time-cards.html', controller: 'TimeCards'});
+  $routeProvider.when('/set-up', {templateUrl: 'partials/set-up.html', controller: 'SetUp'});
   $routeProvider.otherwise({redirectTo: '/clock'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,18 @@
 
 angular.module('myApp.controllers', []).
   controller('EmployeeList', ['$scope', function($scope) {
-$scope.currentUser={'name': "Steve Castle", 'status': "Clocked In"};
+$scope.currentUser={'name': "Steve Castle", 'status': "Clocked In", 'isManager': true};
 $scope.employees=[{"name":"Steve Castle", "department": "Creative", "title": "Manager", "status": "Clocked In"},{"name":"John Finley", "department": "Creative", "title": "Manager", "status": "At Lunch"}];
 $scope.departments=[{"name":"Creative"},{"name":"Sales"}];
 $scope.titles=[{"name":"Manager"},{"name":"Sales Guy"}];
 
-$scope.clockIn = function(){
-alert("You Clocked In");
+$scope.clockIn = function(status){
+$scope.currentUser.status=status;
+$scope.currentUser.statusType="alert-success";
 };
-$scope.clockOut = function(){
-alert("You Clocked Out");
+$scope.clockOut = function(status){
+$scope.currentUser.status=status;
+$scope.currentUser.statusType="alert-danger";
 };
 
 $scope.addEmployee = function(newName,newDepartment,newTitle){
@@ -25,4 +27,8 @@ $scope.employees.push(newEmployee);
 $scope.employees=[{"name":"Steve Castle", "department": "Creative", "title": "Manager"},{"name":"John Finley", "department": "Sales", "title": "Manager"}];
 $scope.departments=[{"name":"Creative"},{"name":"Sales"}];
 $scope.titles=[{"name":"Manager"},{"name":"Sales Guy"}];
-  }]);
+  }])
+   .controller('SetUp', ['$scope',function($scope) {
+$scope.departments=[{"name":"Creative"},{"name":"Sales"}];
+$scope.titles=[{"name":"Manager"},{"name":"Sales Guy"}];
+  }]) ;
